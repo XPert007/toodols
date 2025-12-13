7892949963aade315ca2e7da78da6f2ac4c8dcb0
XPert007 <cookingcapybara@proton.me>
2025-12-14 01:01:45 +0530

    test

---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use crate::event::Event;
+use color_eyre::eyre::{Ok, Result};
+use ratatui::crossterm::style::Color;
+use ratatui::layout::{Constraint, Layout, Rect};
+use ratatui::style::Stylize;
+use ratatui::widgets::{Block, List, ListItem};
+use ratatui::{
+    DefaultTerminal, Frame,
+    crossterm::event,
+    widgets::{Paragraph, Widget},
+};
+#[derive(Debug, Default)]
+struct AppState {
+    items: Vec<TodoItem>,
+}
+
+#[derive(Debug, Default)]
+struct TodoItem {
+    is_done: bool,
+    description: String,
+}
+
+fn main() -> Result<()> {
+    let mut state = AppState::default();
+    color_eyre::install()?;
+    state.items.push(TodoItem {
+        is_done: true,
+        description: "Hello".to_string(),
+    });
+    state.items.push(TodoItem {
+        is_done: false,
+        description: "jediowajer".to_string(),
+    });
+    let terminal = ratatui::init();
+    let result = run(terminal, &mut state);
+    ratatui::restore();
+    result
+}
+
+fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
+    loop {
+        terminal.draw(|f| render(f, app_state))?;
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                event::KeyCode::Esc => {
+                    break;
+                }
+                _ => {}
+            }
+        }
+    }
+    Ok(())
+}
+
+fn render(frame: &mut Frame, app_state: &AppState) {
+    let [border_area] = Layout::vertical([Constraint::Fill(1)])
+        .margin(1)
+        .areas(frame.area());
+    let [inner_area] = Layout::vertical([Constraint::Fill(1)])
+        .margin(1)
+        .areas(border_area);
+    Block::bordered()
+        .border_type(ratatui::widgets::BorderType::Rounded)
+        .fg(Color::Yellow)
+        .render(border_area, frame.buffer_mut());
+    List::new(
+        app_state
+            .items
+            .iter()
+            .map(|x| ListItem::from(x.description.clone())),
+    )
+    .render(inner_area, frame.buffer_mut());
+}
