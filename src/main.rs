af9d1e8eeea48182cb9249e99bb7b39eb62e6f7d
XPert007 <cookingcapybara@proton.me>
2025-12-15 23:07:27 +0530

    added input buffer

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use ratatui::crossterm::event::KeyEvent;
 use ratatui::layout::{Constraint, Layout, Rect};
 use ratatui::style::Style as RStyle;
 use ratatui::style::{Color, Stylize};
-use ratatui::widgets::{Block, List, ListItem, ListState};
+use ratatui::widgets::{Block, BorderType, List, ListItem, ListState, Padding};
 use ratatui::{
     DefaultTerminal, Frame,
     crossterm::event,
@@ -16,6 +16,7 @@ struct AppState {
     items: Vec<TodoItem>,
     list_state: ListState,
     is_add_new: bool,
+    input_value: String,
 }
 
 #[derive(Debug, Default)]
@@ -46,36 +47,63 @@ fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
     loop {
         terminal.draw(|f| render(f, app_state))?;
         if let Event::Key(key) = event::read()? {
-            match key.code {
-                event::KeyCode::Esc => {
+            if app_state.is_add_new {
+                if handle_add_new(key, app_state) {
+                    app_state.is_add_new = false;
+                }
+            } else {
+                if handle_key(key, app_state) {
                     break;
                 }
-                event::KeyCode::Char(char) => match char {
-                    'j' => {
-                        app_state.list_state.select_next();
-                    }
-                    'A' => {
-                        app_state.is_add_new = true;
-                    }
-                    'D' => {
-                        if let Some(index) = app_state.list_state.selected() {
-                            app_state.items.remove(index);
-                        }
-                    }
-                    'k' => {
-                        app_state.list_state.select_previous();
-                    }
-                    _ => {}
-                },
-                _ => {}
             }
         }
     }
     Ok(())
 }
-
-fn handle_key(key: KeyEvent, app_state: &mut AppState) {}
-
+fn handle_add_new(key: KeyEvent, app_state: &mut AppState) -> bool {
+    match key.code {
+        event::KeyCode::Char(c) => {
+            app_state.input_value.push(c);
+        }
+        event::KeyCode::Backspace => {
+            app_state.input_value.pop();
+        }
+        event::KeyCode::Esc => {
+            return true;
+        }
+        event::KeyCode::Enter => {
+            return true;
+        }
+        _ => {}
+    }
+    false
+}
+fn handle_key(key: KeyEvent, app_state: &mut AppState) -> bool {
+    match key.code {
+        event::KeyCode::Esc => {
+            return true;
+        }
+        event::KeyCode::Char(char) => match char {
+            'j' => {
+                app_state.list_state.select_next();
+            }
+            'A' => {
+                app_state.is_add_new = true;
+            }
+            'D' => {
+                if let Some(index) = app_state.list_state.selected() {
+                    app_state.items.remove(index);
+                }
+            }
+            'k' => {
+                app_state.list_state.select_previous();
+            }
+            _ => {}
+        },
+        _ => {}
+    }
+    false
+}
 fn render(frame: &mut Frame, app_state: &mut AppState) {
     let [border_area] = Layout::vertical([Constraint::Fill(1)])
         .margin(1)
@@ -91,9 +119,19 @@ fn render(frame: &mut Frame, app_state: &mut AppState) {
         app_state
             .items
             .iter()
-            .map(|x| ListItem::from(x.description.clone())),
+            .map(|x| ListItem::from(x.description.as_str())),
     )
     .highlight_symbol(">")
     .highlight_style(RStyle::default().fg(Color::Green));
     frame.render_stateful_widget(list, inner_area, &mut app_state.list_state);
+    if app_state.is_add_new {
+        Paragraph::new(app_state.input_value.as_str())
+            .block(
+                Block::bordered()
+                    .fg(Color::Green)
+                    .padding(Padding::uniform(1))
+                    .border_type(BorderType::Rounded),
+            )
+            .render(inner_area, frame.buffer_mut());
+    }
 }
